1e78dd2d850a142b76b023a0ed5cae0d7577fc40
Xiva42 <marcus.dalentoft@gmail.com>
2025-09-18 08:56:26 +0200

    Started to make the interests_relation dictionary

--- main_code.py
+++ main_code.py
@@ -21,9 +21,36 @@ from math import floor as flr
 output = []
 
 
-#################################################### Tuples ######################################################
+#################################################### Tuples, lists & Dictionaries #################################
+# Tuples
+    # This tuple contains the different information titles in the order that it is stored in the google sheets document
 keys_in_order = ("primaryTitle", "runtimeSeconds", "interests", "link", "location")
 
+# Dictionaries
+    # This dictionary contains all the imdb interestes (genres and subgenres) and what genre counterpart it has in the google sheets document
+Interests_relation = {"Action"      : ("Action", "Action Epic", "B-Action", "Car Action", "Martial Arts", "One-Person Army Action", "Superhero", "Sword & Sandal", "War", "War Epic", "Gun Fu", "Kung Fu", "Samurai", "Wuxia"),
+                      "Thriller"    : (),
+                      "Comedy"      : (),
+                      "Adventure"   : ("Adventure", "Desert Adventure", "Dinosaur Adventure", "Adventure Epic", "Globetrotting Adventure", "Jungle Adventure", "Mountain Adventure", "Quest", "Road Trip", "Sea Adventure", "Swashbuckler", "Teen Adventure", "Urban Adventure"),
+                      "Sci-Fi"      : (),
+                      "Fantasy"     : (),
+                      "War"         : (),
+                      "Mystery"     : (),
+                      "Crime"       : (),
+                      "Drama"       : (),
+                      "Agent"       : (),
+                      "Dystopia"    : (),
+                      "Vampire"     : (),
+                      "Western"     : (),
+                      "One-Man-Army": ("One-Person Army Action", ),
+                      "Christmas"   : (),
+                      "Romance"     : (),
+                      "Epic"        : (),
+                      "Superhero"   : (),
+                      "Disaster"    : (),
+                      "Gangster"    : (),
+                      "Pirate"      : ("Swashbuckler", )
+                     }
 
 #################################################### Functions ####################################################
 
@@ -31,7 +58,7 @@ keys_in_order = ("primaryTitle", "runtimeSeconds", "interests", "link", "locatio
 
 #################################################### HTTP Getter ##################################################
 # This makes the request to a imdb api that allows us to GET data with the imdb ID.
-# imdb api link: "https://imdbapi.dev/"
+    # imdb api link: "https://imdbapi.dev/"
 h = hlib.Http(".cache")
 (resp, content) = h.request("https://api.imdbapi.dev/titles/tt0172495", "GET")
 
