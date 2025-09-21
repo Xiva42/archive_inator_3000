36276ef5a7d81686a63d7e2fbef79769f9849465
Xiva42 <marcus.dalentoft@gmail.com>
2025-09-22 00:45:26 +0200

    Made a simple GUI, all exctract functions work except extract_location

---
+++ modules/data_fields.py
@@ -0,0 +1,132 @@
+#################################################### Standard Imports #############################################
+import FreeSimpleGUI as sg
+
+#################################################### Module Imports ###############################################
+from modules import functions as f
+
+
+###################################################################################################################
+#################################################### Variables ####################################################
+###################################################################################################################
+# Different web address roots.
+IMDB_MOVIE_ROOT_ADDRESS = "https://www.imdb.com/title/"
+IMDB_API_TITLE_ROOT_ADRESS = "https://api.imdbapi.dev/titles/"
+
+# GUI related constants.
+APPLICATION_THEME = "DarkGrey9"
+# Colors
+COLOR_RED = "Red"
+COLOR_LIGHT_GREEN = "LightGreen"
+COLOR_GOLD = "Gold"
+###################################################################################################################
+#################################################### Variables ####################################################
+###################################################################################################################
+debug = 0
+
+###################################################################################################################
+#################################################### Tuples, lists & Dictionaries #################################
+###################################################################################################################
+
+# Lists
+    # This list will contain the output message and will be joined with \t in the end.
+output = []
+    # This list contains all the super_genres that will be added to the output list.
+final_super_genres = []
+    # Defines all the elements in the link_promt (main) window.
+    #Sets the GUI theme
+sg.theme(APPLICATION_THEME)
+window_layout = [
+                [sg.Titlebar("The Archive Inator 3000")],
+                [sg.Text("Insert the IMDb link into the box below")],
+                [sg.Input(key="-link-")],
+                [sg.Text("Error: Not a valid link", visible=False, text_color=COLOR_RED, key="-error_msg-")],
+
+                [sg.Text("")],
+
+                [sg.Text("Choose what we have this movie \"stored\" on :)")],
+                [sg.Combo(["DVD", "Blu-ray", "Server", "Other"], default_value="DVD", readonly=True, enable_events=True, key="-location-"),
+                sg.Input(size=(15,1), visible=False, key="-other_location-")],
+                
+                [sg.Text("")],
+                [sg.Push(), sg.Button("Comfirm", enable_events=True, key="-confirm-"),  sg.Push()],
+                [sg.Frame("Success!",
+                        [[sg.Text("The wanted data on:", text_color=COLOR_LIGHT_GREEN),
+                        sg.Text("", key="-subject_movie-", text_color=COLOR_GOLD)],
+                        [sg.Text("is now in your clipboard", text_color=COLOR_LIGHT_GREEN)],
+                        [sg.Text("You can repeat the process if needed :)")]
+                        ], title_color=COLOR_LIGHT_GREEN, visible=False, key="-success_frame-")]
+                ]
+
+#, "location":f.extract_location
+
+# Dictionaries
+    # This dictionary contains the order of data in the output as keys and the relative extract function as the value.
+extracton_order = {"title":f.extract_title, "runtime":f.extract_runtime, "super_genres":f.extract_super_genres, "imdb_link":f.extract_link}
+    # This dictionary contains all the super_genres as the key and its sub_genre relatives as its values.
+                                # Super Genres      # Sub Genres
+super_and_sub_genre_relation =  {"Action"           : ("Action", "Action Epic", "B-Action", "Car Action", "Disaster", "Martial Arts", "One-Person Army Action", "Superhero", "Sword & Sandal", "War", "War Epic", "Gun Fu", "Kung Fu", "Samurai", "Wuxia"),
+                                "Adventure"         : ("Adventure", "Desert Adventure", "Dinosaur Adventure", "Adventure Epic", "Globetrotting Adventure", "Jungle Adventure", "Mountain Adventure", "Quest", "Road Trip", "Sea Adventure", "Swashbuckler", "Teen Adventure", "Urban Adventure"),
+                                "Spy"               : ("Spy"),
+                                "Animation"         : ("Animation", "Adult Animation", "Computer Animation", "Hand-Drawn Animation", "Stop Motion Animation"),
+                                "Anime"             : ("Anime", "Isekai", "Iyashikei", "Josei", "Mecha", "Seinen", "Shōnen", "Shōjo", "Slice of Life"),
+                                "Comedy"            : ("Comedy", "Buddy Comedy", "Buddy Cop", "Dark Comedy", "Farce", "High-Concept Comedy", "Mockumentary", "Parody", "Quirky Comedy", "Raunchy Comedy", "Satire", "Screwball Comedy", "Sitcom", "Sketch Comedy", "Slapstick", "Stand-Up", "Stoner Comedy", "Teen Comedy"),
+                                "Con"               : ("Caper", "Heist"),
+                                "Crime"             : ("Crime", "Caper", "Cop Drama", "Drug Crime", "Film Noir", "Gangster", "Heist", "Police Procedural", "True Crime"),
+                                "Disaster"          : ("Disaster"),
+                                "Documentary"       : ("Documentary", "Crime Documentary", "Docuseries", "Faith & Spirituality Documentary", "Food Documentary", "History Documentary", "Military Documentary", "Music Documentary", "Nature Documentary", "Political Documentary", "Science & Technology Documentary", "Sports Documentary", "Travel Documentary"),
+                                "Drama"             : ("Drama", "Biography", "Coming-of-Age", "Costume Drama", "Docudrama", "Epic", "Financial Drama", "History", "Legal Drama", "Medical Drama", "Period Drama", "Political Drama", "Prison Drama", "Psychological Drama", "Showbiz Drama", "Soap Opera", "Teen Drama", "Tragedy", "Workplace Drama", "Korean Drama", "Telenovela", "Cop Drama"),
+                                "Dystopia"          : ("Dystopian Sci-Fi", "Cyberpunk"),
+                                "Epic"              : ("Epic", "Action Epic", "War Epic", "Historical Epic", "Fantasy Epic", "Romantic Epic", "Sci-Fi Epic", "Western Epic"),
+                                "Fantasy"           : ("Fantasy", "Dark Fantasy", "Fantasy Epic", "Fairy Tale", "Supernatural Fantasy", "Sword & Sorcery", "Teen Fantasy"),
+                                "Gangster"          : ("Gangster"),
+                                "Holiday"           : ("Holiday", "Holiday Animation", "Holiday Comedy", "Holiday Family", "Holiday Romance"),
+                                "Horror"            : ("Horror", "B-Horror", "Body Horror", "Folk Horror", "Found Footage Horror", "Monster Horror", "Psychological Horror", "Slasher Horror", "Splatter Horror", "Supernatural Horror", "Teen Horror", "Vampire Horror", "Werewolf Horror", "Witch Horror", "Zombie Horror"),
+                                "Music"             : ("Music", "Concert"),
+                                "Musical"           : ("Musical", "Classic Musical", "Jukebox Musical", "Pop Musical", "Rock Musical"),
+                                "Mystery"           : ("Mystery", "Bumbling Detective", "Cozy Mystery", "Hard-boiled Detective", "Suspense Mystery", "Whodunnit"),
+                                "One-Man-Army"      : ("One-Person Army Action"),
+                                "Romance"           : ("Romance", "Dark Romance", "Romantic Epic", "Feel-Good Romance", "Romantic Comedy", "Steamy Romance", "Teen Romance", "Tragic Romance"),
+                                "Sci-Fi"            : ("Sci-Fi", "Alien Invasion", "Artificial Intelligence", "Cyberpunk", "Kaiju", "Space Sci-Fi", "Steampunk", "Time Travel"),
+                                "Superhero"         : ("Superhero"),
+                                "Sport"             : ("Sport", "Baseball", "Basketball", "Boxing", "Extreme Sport", "Football", "Motorsport", "Soccer", "Water Sport"),
+                                "Swashbuckler"      : ("Swashbuckler"),
+                                "Time Travel"       : ("Time Travel"),
+                                "Thriller"          : ("Thriller", "Conspiracy Thriller", "Cyber Thriller", "Erotic Thriller", "Giallo", "Legal Thriller", "Political Thriller", "Psychological Thriller", "Serial Killer", "Spy", "Survival"),
+                                "Vampire"           : ("Vampire Horror"),
+                                "War"               : ("War", "War Epic"),
+                                "Western"           : ("Western", "Classical Western", "Contemporary Western", "Western Epic", "Spaghetti Western")
+                                }
+    # This dictionary keeps track of which super_genres already has been added to the final_genres list, to avoid duplicates.
+already_added_super_genre =     {
+                                "Action"            : False,
+                                "Adventure"         : False,
+                                "Spy"               : False,
+                                "Animation"         : False,
+                                "Anime"             : False,
+                                "Comedy"            : False,
+                                "Con"               : False,
+                                "Crime"             : False,
+                                "Disaster"          : False,
+                                "Documentary"       : False,
+                                "Drama"             : False,
+                                "Dystopia"          : False,
+                                "Epic"              : False,
+                                "Fantasy"           : False,
+                                "Gangster"          : False,
+                                "Holiday"           : False,
+                                "Horror"            : False,
+                                "Music"             : False,
+                                "Musical"           : False,
+                                "Mystery"           : False,
+                                "One-Man-Army"      : False,
+                                "Romance"           : False,
+                                "Sci-Fi"            : False,
+                                "Superhero"         : False,
+                                "Sport"             : False,
+                                "Swashbuckler"      : False,
+                                "Time Travel"       : False,
+                                "Thriller"          : False,
+                                "Vampire"           : False,
+                                "War"               : False,
+                                "Western"           : False
+                                }
---
+++ modules/functions.py
@@ -0,0 +1,95 @@
+#################################################### Standard Imports #############################################
+import httplib2 as hlib
+import json
+from math import floor as flr
+
+###################################################################################################################
+#################################################### Functions ####################################################
+###################################################################################################################
+
+#################################################### Extraction Functions #########################################
+    #This function extracts and appends the title to output.
+def extract_title(src:dict) -> None:
+    title = src["primaryTitle"]
+    d.output.append(title)
+
+    #This function extracts and appends the length in hours and minutes to output.
+def extract_runtime(src:dict) -> None:
+    length_in_seconds = src["runtimeSeconds"]
+    minutes = int(flr(length_in_seconds/60))
+    hours = int(flr(minutes/60))
+    extra_minutes = minutes - hours*60
+
+    # Concatenates the hours and minutes with letters in between and appends the movie length to output.
+    lenght_str = str(hours) + "h " + str(extra_minutes) + "m"
+    d.output.append(lenght_str)
+
+    # Extracts and appends relevant super_genres to output.
+def extract_super_genres(src:dict) -> None:
+    for interest_dict in src["interests"]:
+        interest_name = interest_dict["name"]
+
+        # This functions finds and returns the relative super_genre to the current interest.
+        relative_super_genre = find_reletive_super_genre(interest_name)
+        if relative_super_genre != None:
+            # this sets the found super_genres relative value in the already_added_super_genre list to True.
+            d.already_added_super_genre[relative_super_genre] = True
+            # This appends the relative super_genre to the final_super_genres.
+            d.final_super_genres.append(relative_super_genre)
+
+    # This formats and appends the list of super_genres to the output list.
+    genre_str = ", ".join(d.final_super_genres)
+    d.output.append(genre_str)
+
+    # Extracts and appends the link for this movies imdb site.
+def extract_link(src:dict) -> None:
+    movie_id = src["id"]
+    imdb_link = d.IMDB_MOVIE_ROOT_ADDRESS + movie_id
+    d.output.append(imdb_link)
+
+    # Extracts and appends the chosen location of the movie.
+#def extract_location(src:dict) -> None:
+#    d.output.append(src)
+
+
+#################################################### Other Functions ##############################################
+# This modifies the link to go through the imdb api, makes the request to the given link and return it as a python dictionary.
+def http_get_request(http:hlib.Http, link:str) -> dict:
+    # Finds the imdb id in the given link.
+    id_start = len(d.IMDB_MOVIE_ROOT_ADDRESS)
+    id = link[id_start:id_start+9]
+    link = d.IMDB_API_TITLE_ROOT_ADRESS + id
+
+    # This makes the http request and gets the data as json.
+    (resp, content) = http.request(link, "GET")
+    # Converts the bytes recived into a python dictionary.
+    data = json.loads(content)
+    return data
+
+    
+
+# This function finds the super_genre that corrosponds with the given interest and returns it.
+def find_reletive_super_genre(interest:str) -> str:
+    for super_genre in d.super_and_sub_genre_relation:
+        # This checks if the super_genre already have been added to the final_super_genre list so it doesn't add duplicate genres.
+        if d.already_added_super_genre[super_genre]:
+            pass
+        else:
+            # Gets the relative sub_genres (in a tuple) for the current super_genre.
+            relative_sub_genres = d.super_and_sub_genre_relation[super_genre]
+
+            # This means that there is more than one relative sub_genre, which means we need to iterate them.
+            if type(relative_sub_genres) == tuple:
+                # This compares all the sub_genres from the current super_genre with the current interest and returns the super_genre if it matches.
+                for sub_genre in relative_sub_genres:
+                    if interest == sub_genre:
+                        return super_genre
+
+            # This means there is only one sub_genre, and therefore the sub_genre and super_genre is the same (sub_genre = super_genre).
+            elif type(relative_sub_genres) == str:
+                # This compares the sub_genre from the current super_genre with the current interest and returns the super_genre if it matches.
+                if interest == relative_sub_genres:
+                        return super_genre
+
+#################################################### Module Imports ###############################################
+from modules import data_fields as d
