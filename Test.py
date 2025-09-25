c74d5d783eece319b7c3920ee6251c2a3642c87e
Xiva42 <marcus.dalentoft@gmail.com>
2025-09-25 03:07:52 +0200

    added a test file

---
+++ Test.py
@@ -0,0 +1,11 @@
+list = ["a","b","c"]
+wubs = ["d","e"]
+
+print(list)
+print(wubs)
+
+wubs = "".join(list)
+
+print(list)
+print(wubs)
+
--- main.py
+++ main.py
@@ -2,79 +2,101 @@
     # super_genre   ->  The genres wanted and is to be used in the google sheets document.
     # sub_genre     ->  The genres that IMDb have, these will all have a relative super_genres which it will translated to.
     # interest      ->  The genres the application recives with the given movie (thats what they are called in the HTTP response).
-#################################################### Standard Imports #############################################
+#################################################### Standard Imports ###########################################################
 import httplib2 as hlib
 import FreeSimpleGUI as sg
 import pyperclip as pyclp
 import webbrowser as webb
-#################################################### Module Imports ###############################################
+#################################################### Module Imports #############################################################
 from modules import functions as f
 from modules import data_fields as d
 from modules import gui_elements as gui
+from modules.enums import *
 
 
-###################################################################################################################
-#################################################### Application Loop #############################################
-###################################################################################################################
+#################################################################################################################################
+#################################################### Application Loop ###########################################################
+#################################################################################################################################
 # Initiates the http class.
 h = hlib.Http(".cache")
 
 #Initates the window with the gui_layout list.
 window = sg.Window("The Archive Inator 3000", gui.window_layout, keep_on_top=True, finalize=True)
 # Enables the enter key to act as a "confirm" button.
-window.bind("<Return>", "-enter_key-")
+window.bind("<Return>", GuiKey.ENTER_KEY)
 
 # The main application loop which reads events and inputs into the GUI and makes operations with the input.
 while True:
     event, values = window.read()
     print(event, values)
-
     if event == sg.WIN_CLOSED:
         break
 
-    elif event == "-xiva_link-":
+
+    elif event == GuiKey.XIVA_LINK:
         webb.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ",2)
 
-    elif event == "-location-":
-        if values["-location-"] == "Other":
-            window["-other_location-"].update(visible=True)
-            
-        elif window["-other_location-"].visible:
-            window["-other_location-"].update(visible=False)            
 
-    elif event == "-confirm-" or "-enter_key-":
-        try:
-            ######################################## HTTP Getter ##################################################
+    elif event == GuiKey.MEDIA:
+        d.media_type = values[GuiKey.MEDIA]
+        window[GuiKey.LOCATION_TXT].update(f"Select the {d.media_type} \"storage location\":")
+
+        if values[GuiKey.MEDIA] == MediaType.OTHER.value:
+            window[GuiKey.MEDIA_OTHER].update(visible=True)
+            
+        elif window[GuiKey.MEDIA_OTHER].visible:
+            window[GuiKey.MEDIA_OTHER].update(visible=False)
+
+
+    elif event == GuiKey.LOCATION:
+        if values[GuiKey.LOCATION] == Location.OTHER.value:
+            window[GuiKey.LOCATION_OTHER].update(visible=True)
+            
+        elif window[GuiKey.LOCATION_OTHER].visible:
+            window[GuiKey.LOCATION_OTHER].update(visible=False)            
+
+
+    elif event == GuiKey.CONFIRM or event == GuiKey.ENTER_KEY:
+        # try:
+            ######################################## HTTP Getter ################################################################
             # This makes the request to a imdb api that allows us to GET data with the user given imdb ID.
-            given_link = values["-link-"]
+            given_link = values[GuiKey.IMDB_LINK]
             data_dict = f.http_get_request(h, given_link)
             
-            ######################################## Data extraction and formatting ###############################
-            # This inserts the user given "location" for the movie into the data_dict.
-            data_dict["storageLocation"] = {"location": values["-location-"], "other_location": values["-other_location-"]}
-
+            ######################################## Data extraction and formatting #############################################
+            
             # This runs all the extraction functions which extracts the wanted data and appends it in the given extraction_order.
-            for i in d.extracton_order:
-                d.extracton_order[i](data_dict)
+            for func in Extraction:
 
-            ######################################## Data String Export ###########################################
+                if func == Extraction.LOCATION:
+                    # This inserts the user given "location" for the media into the data_dict.
+                    data_dict[DataKey.LOCATION] = {DataKey.LOCATION_COMMON: values[GuiKey.LOCATION], DataKey.LOCATION_OTHER: values[GuiKey.LOCATION_OTHER]}
+                elif func == Extraction.MEDIA:
+                    # This inserts the user given "media" into the data_dict.
+                    data_dict[DataKey.MEDIA] = {DataKey.MEDIA_COMMON: values[GuiKey.MEDIA], DataKey.MEDIA_OTHER: values[GuiKey.MEDIA_OTHER]}
+
+                func.value(data_dict)
+
+            ######################################## Data String Export #########################################################
             # Joins the output list with tab as separator so it can be pasted easily into google sheet.
             str_output = "\t".join(d.output)
             pyclp.copy(str_output)
 
-            ######################################## Success reset ################################################
+            ######################################## Success reset ##############################################################
             # Hides the Error message if it was there.
-            window["-error_msg-"].update(visible=False)
+            window[GuiKey.ERROR_MSG].update(visible=False)
             # Removes the link from the input box.
-            window["-link-"].update(value="")
+            window[GuiKey.IMDB_LINK].update(value="")
             # shows the retrieved Movie's name in the success_frame.
-            window["-subject_movie-"].update(value=d.output[0])
+            window[GuiKey.SUBJECT_MEDIA].update(value=d.output[0])
             # Displays the success_frame.
-            window["-success_frame-"].update(visible=True)
+            window[GuiKey.SUCCESS_FRAME].update(visible=True)
+            # Empties the output list.
+            d.output = []
         
         # If the link given didnt work or wasn't a link this makes sure the program doesn't crash.
-        except Exception as e:
-            print("Error: ", e)
-            window["-error_msg-"].update(visible=True)
+        # except Exception as e:
+        #     print("Error: ", e)
+        #     window[GuiKey.ERROR_MSG].update(visible=True)
 
 window.close()
